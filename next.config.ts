69e42c3a4adbdea4ff56df1ddc5e512004611c77
amitvaibhavtiwari <amitvaibhavtiwari@gmail.com>
2026-06-30 19:57:22 +0000

    feat: Fixed the version number for dependencies to be exact and addded some starter files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  reactStrictMode: true,
+  typedRoutes: true,
+  serverExternalPackages: ["kysely", "pg", "better-sqlite3"],
 };
 
 export default nextConfig;
